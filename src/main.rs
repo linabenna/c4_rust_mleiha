2bb22a5c5db6c4170b59c2cd34de568e33d6cf8a
linabenna <linabenna1@gmail.com>
2025-05-04 14:07:05 +0400

    added more operations like arithmetics and comment handling

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,9 @@ enum Token {
     Num(i64),
     Id(String),
     Char(char),
+    Str(String),
     Else, Enum, If, Int, Return, Sizeof, While, Assign, Cond, Lor, Lan, Or,
-    Xor, And, Eq, Ne, Lt, Gt, Ge, Shl, Shr, Add, Sub, Mul, Div, Mod, Inc, Dec,
+    Xor, And, Eq, Ne, Lt, Le, Gt, Ge, Shl, Shr, Add, Sub, Mul, Div, Mod, Inc, Dec,
     Brak, LParen, RParen, LBrace, RBrace, Comma, Colon, Semicolon, RBrak,
 }
 
@@ -68,6 +69,16 @@ impl<'a> Lexer<'a> {
         self.position += 1; // move the reading position to the next 
     }
 
+    // this function allows us to look at the next character in the source code 
+    // without actually advancing the current reading position (used for lookahead logic)
+    fn peek(&self) -> Option<char> { // sampe implementation as advance() method 
+        if self.position < self.source.len() {
+            Some(self.source.as_bytes()[self.position] as char)
+        } else {
+            None
+        }
+    }
+
     // this function gets the next token from the source code
     fn next_token(&mut self) -> Option<Token> {
         while let Some(c) = self.current_char { // loop while there is a current character to process
@@ -77,6 +88,112 @@ impl<'a> Lexer<'a> {
                     self.line += 1; // then increment line number 
                     self.advance();
                 }
+
+                // handle single-line comments and hash comments
+                '/' => {
+                    if self.peek() == Some('/') {
+                        while self.current_char != Some('\n') && self.current_char.is_some() {
+                            self.advance();
+                        }
+                    } else {
+                        self.advance();
+                        return Some(Token::Div);
+                    }
+                }
+                '#' => {
+                    while self.current_char != Some('\n') && self.current_char.is_some() {
+                        self.advance();
+                    }
+                }
+
+                // handle string literal
+                '"' => {
+                    self.advance();
+                    let mut string = String::new();
+                    while let Some(ch) = self.current_char {
+                        if ch == '"' {
+                            break;
+                        }
+                        string.push(ch);
+                        self.advance();
+                    }
+                    self.advance(); // skip closing "
+                    return Some(Token::Str(string));
+                }
+
+                // handle character literal
+                '\'' => {
+                    self.advance();
+                    let ch = self.current_char?;
+                    self.advance();
+                    self.advance(); // skip closing '
+                    return Some(Token::Char(ch));
+                }
+
+                // handle operators
+                '=' => {
+                    self.advance();
+                    if self.current_char == Some('=') {
+                        self.advance();
+                        return Some(Token::Eq);
+                    }
+                    return Some(Token::Assign);
+                }
+
+                '!' => {
+                    self.advance();
+                    if self.current_char == Some('=') {
+                        self.advance();
+                        return Some(Token::Ne);
+                    }
+                }
+
+                '<' => {
+                    self.advance();
+                    if self.current_char == Some('=') {
+                        self.advance();
+                        return Some(Token::Le);
+                    }
+                    return Some(Token::Lt);
+                }
+
+                '>' => {
+                    self.advance();
+                    if self.current_char == Some('=') {
+                        self.advance();
+                        return Some(Token::Ge);
+                    }
+                    return Some(Token::Gt);
+                }
+
+                '+' => {
+                    self.advance();
+                    if self.current_char == Some('+') {
+                        self.advance();
+                        return Some(Token::Inc);
+                    }
+                    return Some(Token::Add);
+                }
+
+                '-' => {
+                    self.advance();
+                    if self.current_char == Some('-') {
+                        self.advance();
+                        return Some(Token::Dec);
+                    }
+                    return Some(Token::Sub);
+                }
+
+                '*' => {
+                    self.advance();
+                    return Some(Token::Mul);
+                }
+
+                '%' => {
+                    self.advance();
+                    return Some(Token::Mod);
+                }
+
                 '0'..='9' => return Some(self.lex_number()), // if a digit is found, parse a number token
 
                 // if a letter or underscore is found, parse an identifier or keyword
@@ -136,7 +253,7 @@ impl<'a> Lexer<'a> {
 }
 
 fn main() {
-    let source_code = "int main() {  printf('hello, world\n'); }";
+    let source_code = "int main() {  5 + 2; }";
     // let source_code = "int main() {  return 0; }";
     let mut lexer = Lexer::new(source_code);
 
